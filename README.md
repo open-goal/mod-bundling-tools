805e26f790af31d131b11807a29ebccef9f1cf39
Tyler Wilding <xtvaser@gmail.com>
2023-07-14 13:22:29 -0600

    bundler: emit metadata file as an artifact

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # OpenGOAL Mod Distribution Tools
 
+**This is still being actively developed and may change, use at your own risk**
+
 This repo contains some github actions, scripts, and JSON schemas to attempt to simplify and standardize OpenGOAL mod formats.
 
 Note that overtime this action may change as the underlying expected structure of mods change.  One of the benefits of using these actions is that complying with said changes should be as easy as using the latest version.
---
+++ scripts/mod-bundler/emit-metadata.py
@@ -0,0 +1,23 @@
+# Simple script to emit a metadata.json file with the relevant mod information
+# Values are passed in as environment variables
+from datetime import datetime
+
+def split_comma_sep_val(str):
+    if "," not in str:
+        return [str]
+    return str.split(",")
+
+metadata = {
+    "schemaVersion": os.getenv("SCHEMA_VERSION"),
+    "version": os.getenv("VERSION"),
+    "name": os.getenv("NAME"),
+    "description": os.getenv("DESCRIPTION"),
+    "authors": split_comma_sep_val(os.getenv("AUTHORS")),
+    "tags": split_comma_sep_val(os.getenv("TAGS")),
+    "publishedDate": datetime.now().isoformat()
+}
+if os.getenv("WEBSITE_URL") != "":
+    metadata["websiteUrl"] = os.getenv("WEBSITE_URL")
+
+with open("%s/metadata.json".format(os.getenv("OUT_DIR")), "w", encoding="utf-8") as f:
+    f.write(json.dumps(metadata, indent=2, ensure_ascii=False))
