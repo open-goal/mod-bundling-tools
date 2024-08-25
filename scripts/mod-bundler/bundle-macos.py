76b9844ab852c198bdadc7a18da1d318e5939e33
Tyler Wilding <xTVaser@users.noreply.github.com>
2024-08-25 19:51:07 -0400

    mods: simplify bundler scripts and remove deprecated asset handling (#16)
    * mods: simplify bundler scripts and remove deprecated asset handling
    
    * Update bundle-linux.py
    
    * Update bundle-macos.py
    
    ---------
    
    Co-authored-by: Matt Dallmeyer <mattdallmeyer@gmail.com>

--- scripts/mod-bundler/bundle-macos.py
+++ scripts/mod-bundler/bundle-macos.py
@@ -1,177 +1,27 @@
-import os
-import shutil
-import requests
-import urllib.request
-import zipfile
-import tarfile
+from common import (
+    create_output_dir,
+    download_release,
+    finalize_bundle,
+    get_args,
+    override_binaries_and_assets,
+    patch_mod_timestamp_and_version_info,
+)
 
-args = {
-    "outputDir": os.getenv("outputDir"),
-    "versionName": os.getenv("versionName"),
-    "toolingRepo": os.getenv("toolingRepo"),
-    "toolingVersion": os.getenv("toolingVersion"),
-    "toolingBinaryDir": os.getenv("toolingBinaryDir"),
-    "copyEntireBinaryDir": os.getenv("copyEntireBinaryDir"),
-    "textureReplacementDir": os.getenv("textureReplacementDir"),
-    "customLevelsDir": os.getenv("customLevelsDir"),
-    "customAssetsDir": os.getenv("customAssetsDir"),
-    "goalSourceDir": os.getenv("goalSourceDir"),
-    "gameAssetsDir": os.getenv("gameAssetsDir"),
-    "decompilerConfigDir": os.getenv("decompilerConfigDir"),
-}
+args = get_args()
 
 print(args)
 
 # Create our output directory
-if os.path.exists(os.path.join(args["outputDir"], "macos-intel")):
-    print(
-        "Expected output directory already exists, clearing it - {}".format(
-            os.path.join(args["outputDir"], "macos-intel")
-        )
-    )
-    os.rmdir(os.path.join(args["outputDir"], "macos-intel"))
-
-os.makedirs(os.path.join(args["outputDir"], "macos-intel"), exist_ok=True)
+create_output_dir(args, "macos-intel")
 
 # Download the Release
-toolingRepo = args["toolingRepo"]
-toolingVersion = args["toolingVersion"]
-if toolingVersion == "latest":
-    # Get the latest release
-    toolingVersion = requests.get(
-        f"https://api.github.com/repos/{toolingRepo}/releases/latest"
-    ).json()["tag_name"]
-releaseAssetUrl = f"https://github.com/{toolingRepo}/releases/download/{toolingVersion}/opengoal-macos-intel-{toolingVersion}.tar.gz"
-urllib.request.urlretrieve(
-    releaseAssetUrl, os.path.join(args["outputDir"], "macos-intel", "release.tar.gz")
-)
+download_release(args, "macos-intel", is_zip=False)
 
-# Extract it
-with tarfile.open(
-    os.path.join(args["outputDir"], "macos-intel", "release.tar.gz")
-) as tar_ball:
-    tar_ball.extractall(os.path.join(args["outputDir"], "macos-intel"))
-os.remove(os.path.join(args["outputDir"], "macos-intel", "release.tar.gz"))
+# Override any files
+override_binaries_and_assets(args, "macos-intel", executable_extensions=False)
 
-
-if args["toolingBinaryDir"] != "":
-    # User is specifying the binaries themselves, let's make sure they exist
-    dir = args["toolingBinaryDir"]
-    if (
-        not os.path.exists(os.path.join(dir, "extractor"))
-        or not os.path.exists(os.path.join(dir, "goalc"))
-        or not os.path.exists(os.path.join(dir, "gk"))
-    ):
-        print("Tooling binaries not found, expecting extractor, goalc, and gk")
-        exit(1)
-
-    # Binaries are all there, let's replace 'em
-
-    if args["copyEntireBinaryDir"] != "" and (args["copyEntireBinaryDir"] == "true" or args["copyEntireBinaryDir"]):
-      # user has some DLLs or something, copy entire binary dir
-      shutil.copytree(
-        dir,
-        os.path.join(args["outputDir"], "macos-intel"),
-        dirs_exist_ok=True
-      )
-    else:
-      # copy the 3 key binaries
-      shutil.copyfile(
-          os.path.join(dir, "extractor"),
-          os.path.join(args["outputDir"], "macos-intel", "extractor"),
-      )
-      shutil.copyfile(
-          os.path.join(dir, "goalc"),
-          os.path.join(args["outputDir"], "macos-intel", "goalc"),
-      )
-      shutil.copyfile(
-          os.path.join(dir, "gk"), os.path.join(args["outputDir"], "macos-intel", "gk")
-      )
-    
-    # permissions
-    os.chmod(os.path.join(args["outputDir"], "macos-intel", "extractor"), 0o775)
-    os.chmod(os.path.join(args["outputDir"], "macos-intel", "goalc"), 0o775)
-    os.chmod(os.path.join(args["outputDir"], "macos-intel", "gk"), 0o775)
-
-# Copy-in Mod Assets
-textureReplacementDir = args["textureReplacementDir"]
-if os.path.exists(textureReplacementDir):
-    shutil.copytree(
-        textureReplacementDir,
-        os.path.join(args["outputDir"], "macos-intel", "data", "texture_replacements"),
-        dirs_exist_ok=True,
-    )
-
-# Old structure
-customLevelsDir = args["customLevelsDir"]
-if os.path.exists(customLevelsDir):
-    shutil.copytree(
-        customLevelsDir,
-        os.path.join(args["outputDir"], "macos-intel", "data", "custom_levels"),
-        dirs_exist_ok=True,
-    )
-
-# New structure
-customAssetsDir = args["customAssetsDir"]
-if os.path.exists(customAssetsDir):
-    shutil.copytree(
-        customAssetsDir,
-        os.path.join(args["outputDir"], "macos-intel", "data", "custom_assets"),
-        dirs_exist_ok=True,
-    )
-
-goalSourceDir = args["goalSourceDir"]
-if not os.path.exists(goalSourceDir):
-    print(
-        "Goal source directory not found at {}, not much of a mod without that!".format(
-            goalSourceDir
-        )
-    )
-    exit(1)
-shutil.copytree(
-    goalSourceDir,
-    os.path.join(args["outputDir"], "macos-intel", "data", "goal_src"),
-    dirs_exist_ok=True,
-)
-
-decompilerConfigDir = args["decompilerConfigDir"]
-if os.path.exists(decompilerConfigDir):
-    shutil.copytree(
-        decompilerConfigDir,
-        os.path.join(args["outputDir"], "macos-intel", "data", "decompiler", "config"),
-        dirs_exist_ok=True,
-    )
-else:
-    print("Decompiler config directory not found at {}, skipping.".format(decompilerConfigDir))
-
-if args["gameAssetsDir"] != "":
-  gameAssetsDir = args["gameAssetsDir"]
-  if not os.path.exists(gameAssetsDir):
-      print(
-          "Game assets directory not found at {}!".format(
-              gameAssetsDir
-          )
-      )
-      exit(1)
-  shutil.copytree(
-      gameAssetsDir,
-      os.path.join(args["outputDir"], "macos-intel", "data", "game", "assets"),
-      dirs_exist_ok=True,
-  )
+# Replace placeholder text with mod version and timestamp
+patch_mod_timestamp_and_version_info(args, "macos-intel")
 
 # Rezip it up and prepare it for upload
-shutil.make_archive(
-    "macos-intel-{}".format(args["versionName"]),
-    "gztar",
-    os.path.join(args["outputDir"], "macos-intel"),
-)
-os.makedirs(os.path.join(args["outputDir"], "dist"), exist_ok=True)
-shutil.move(
-    "macos-intel-{}.tar.gz".format(args["versionName"]),
-    os.path.join(
-        args["outputDir"], "dist", "macos-intel-{}.tar.gz".format(args["versionName"])
-    ),
-)
-
-# Cleanup
-shutil.rmtree(os.path.join(args["outputDir"], "macos-intel"))
+finalize_bundle(args, "macos-intel", is_zip=False)
--- scripts/mod-bundler/bundle-windows.py
+++ scripts/mod-bundler/bundle-windows.py
@@ -1,198 +1,27 @@
-import os
-import shutil
-import requests
-import urllib.request
-import zipfile
-import datetime
+from common import (
+    create_output_dir,
+    download_release,
+    finalize_bundle,
+    get_args,
+    override_binaries_and_assets,
+    patch_mod_timestamp_and_version_info,
+)
 
-args = {
-    "outputDir": os.getenv("outputDir"),
-    "versionName": os.getenv("versionName"),
-    "toolingRepo": os.getenv("toolingRepo"),
-    "toolingVersion": os.getenv("toolingVersion"),
-    "toolingBinaryDir": os.getenv("toolingBinaryDir"),
-    "copyEntireBinaryDir": os.getenv("copyEntireBinaryDir"),
-    "textureReplacementDir": os.getenv("textureReplacementDir"),
-    "customLevelsDir": os.getenv("customLevelsDir"),
-    "customAssetsDir": os.getenv("customAssetsDir"),
-    "goalSourceDir": os.getenv("goalSourceDir"),
-    "gameAssetsDir": os.getenv("gameAssetsDir"),
-    "decompilerConfigDir": os.getenv("decompilerConfigDir"),
-}
+args = get_args()
 
 print(args)
 
 # Create our output directory
-if os.path.exists(os.path.join(args["outputDir"], "windows")):
-    print(
-        "Expected output directory already exists, clearing it - {}".format(
-            os.path.join(args["outputDir"], "windows")
-        )
-    )
-    os.rmdir(os.path.join(args["outputDir"], "windows"))
-
-os.makedirs(os.path.join(args["outputDir"], "windows"), exist_ok=True)
+create_output_dir(args, "windows")
 
 # Download the Release
-toolingRepo = args["toolingRepo"]
-toolingVersion = args["toolingVersion"]
-if toolingVersion == "latest":
-    # Get the latest release
-    toolingVersion = requests.get(
-        f"https://api.github.com/repos/{toolingRepo}/releases/latest"
-    ).json()["tag_name"]
-releaseAssetUrl = f"https://github.com/{toolingRepo}/releases/download/{toolingVersion}/opengoal-windows-{toolingVersion}.zip"
-urllib.request.urlretrieve(
-    releaseAssetUrl, os.path.join(args["outputDir"], "windows", "release.zip")
-)
+download_release(args, "windows", is_zip=True)
 
-# Extract it
-with zipfile.ZipFile(
-    os.path.join(args["outputDir"], "windows", "release.zip"), "r"
-) as zip_ref:
-    zip_ref.extractall(os.path.join(args["outputDir"], "windows"))
-os.remove(os.path.join(args["outputDir"], "windows", "release.zip"))
-
-if args["toolingBinaryDir"] != "":
-    # User is specifying the binaries themselves, let's make sure they exist
-    dir = args["toolingBinaryDir"]
-    if (
-        not os.path.exists(os.path.join(dir, "extractor.exe"))
-        or not os.path.exists(os.path.join(dir, "goalc.exe"))
-        or not os.path.exists(os.path.join(dir, "gk.exe"))
-    ):
-        print(
-            "Tooling binaries not found, expecting extractor.exe, goalc.exe, and gk.exe"
-        )
-        exit(1)
-
-    # Binaries are all there, let's replace 'em
-
-    if args["copyEntireBinaryDir"] != "" and (args["copyEntireBinaryDir"] == "true" or args["copyEntireBinaryDir"]):
-      # user has some DLLs or something, copy entire binary dir
-      shutil.copytree(
-        dir,
-        os.path.join(args["outputDir"], "windows"),
-        dirs_exist_ok=True
-      )
-    else:
-      # copy the 3 key binaries
-      shutil.copyfile(
-          os.path.join(dir, "extractor.exe"),
-          os.path.join(args["outputDir"], "windows", "extractor.exe"),
-      )
-      shutil.copyfile(
-          os.path.join(dir, "goalc.exe"),
-          os.path.join(args["outputDir"], "windows", "goalc.exe"),
-      )
-      shutil.copyfile(
-          os.path.join(dir, "gk.exe"),
-          os.path.join(args["outputDir"], "windows", "gk.exe")
-      )
-
-# Copy-in Mod Assets
-textureReplacementDir = args["textureReplacementDir"]
-if os.path.exists(textureReplacementDir):
-    shutil.copytree(
-        textureReplacementDir,
-        os.path.join(args["outputDir"], "windows", "data", "texture_replacements"),
-        dirs_exist_ok=True,
-    )
-
-# Old structure
-customLevelsDir = args["customLevelsDir"]
-if os.path.exists(customLevelsDir):
-    shutil.copytree(
-        customLevelsDir,
-        os.path.join(args["outputDir"], "windows", "data", "custom_levels"),
-        dirs_exist_ok=True,
-    )
-
-# New structure
-customAssetsDir = args["customAssetsDir"]
-if os.path.exists(customAssetsDir):
-    shutil.copytree(
-        customAssetsDir,
-        os.path.join(args["outputDir"], "windows", "data", "custom_assets"),
-        dirs_exist_ok=True,
-    )
-
-goalSourceDir = args["goalSourceDir"]
-if not os.path.exists(goalSourceDir):
-    print(
-        "Goal source directory not found at {}, not much of a mod without that!".format(
-            goalSourceDir
-        )
-    )
-    exit(1)
-shutil.copytree(
-    goalSourceDir,
-    os.path.join(args["outputDir"], "windows", "data", "goal_src"),
-    dirs_exist_ok=True,
-)
-
-if args["gameAssetsDir"] != "":
-  gameAssetsDir = args["gameAssetsDir"]
-  if not os.path.exists(gameAssetsDir):
-      print(
-          "Game assets directory not found at {}!".format(
-              gameAssetsDir
-          )
-      )
-      exit(1)
-  shutil.copytree(
-      gameAssetsDir,
-      os.path.join(args["outputDir"], "windows", "data", "game", "assets"),
-      dirs_exist_ok=True,
-  )
-
-decompilerConfigDir = args["decompilerConfigDir"]
-if os.path.exists(decompilerConfigDir):
-    shutil.copytree(
-        decompilerConfigDir,
-        os.path.join(args["outputDir"], "windows", "data", "decompiler", "config"),
-        dirs_exist_ok=True,
-    )
-else:
-    print("Decompiler config directory not found at {}, skipping.".format(decompilerConfigDir))
+# Override any files
+override_binaries_and_assets(args, "windows", executable_extensions=True)
 
 # Replace placeholder text with mod version and timestamp
-try:
-  path = os.path.join(args["outputDir"], "windows", "data", "goal_src", "jak1", "engine", "mods", "mod-settings.gc")
-  file = open(path, "r")
-  file_data = file.read()
-  file.close()
-
-  # Check if the placeholder string is present in the file
-  if "%MODVERSIONPLACEHOLDER%" in file_data:
-    # Replace the placeholder string with the version and date string
-    version_str = args["versionName"] + " " + datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
-    file_data = file_data.replace("%MODVERSIONPLACEHOLDER%", version_str)
-
-    # Write the updated content back to the mod-settings
-    file = open(path, "w")
-    file.write(file_data)
-    file.close()
-    print(f"String %MODVERSIONPLACEHOLDER% replaced with '{version_str}' in the file.")
-  else:
-    print(f"Couldn't find %MODVERSIONPLACEHOLDER% in the file.")
-except Exception as e:
-  print(f"Something went wrong trying to replace placeholder text with mod version info:")
-  print(e)
+patch_mod_timestamp_and_version_info(args, "windows")
 
 # Rezip it up and prepare it for upload
-shutil.make_archive(
-    "windows-{}".format(args["versionName"]),
-    "zip",
-    os.path.join(args["outputDir"], "windows"),
-)
-os.makedirs(os.path.join(args["outputDir"], "dist"), exist_ok=True)
-shutil.move(
-    "windows-{}.zip".format(args["versionName"]),
-    os.path.join(
-        args["outputDir"], "dist", "windows-{}.zip".format(args["versionName"])
-    ),
-)
-
-# Cleanup
-shutil.rmtree(os.path.join(args["outputDir"], "windows"))
+finalize_bundle(args, "windows", is_zip=True)
